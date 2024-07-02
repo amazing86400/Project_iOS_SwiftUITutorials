a42c584c5c9e0d6e02fd51a66a99b43ff59ea5fc
kbshin <amazing1997@naver.com>
2024-07-02 17:35:36 +0900

    Initial Commit

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by KIBEOM SHIN on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
