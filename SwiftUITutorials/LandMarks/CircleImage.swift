5da60dcde1d8911b5d4c6355d4c19f2951947e24
kbshin <amazing1997@naver.com>
2024-07-02 18:25:05 +0900

    랜드마크뷰 작업 완료

---
+++ SwiftUITutorials/LandMarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorials
+//
+//  Created by KIBEOM SHIN on 7/2/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
