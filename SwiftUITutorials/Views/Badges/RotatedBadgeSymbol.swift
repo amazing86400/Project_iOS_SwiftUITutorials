9e9c1819baa1dfbf4e4db0e386cf12e2fc212e8a
kbshin <amazing1997@naver.com>
2024-07-10 18:41:19 +0900

    뱃지뷰 추가

---
+++ SwiftUITutorials/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUITutorials
+//
+//  Created by KIBEOM SHIN on 7/10/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
