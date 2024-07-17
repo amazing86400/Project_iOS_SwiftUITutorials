8ee9eb988a82da9bccd39e5d6ead9a02b0a1cdfe
kbshin <amazing1997@naver.com>
2024-07-17 18:14:55 +0900

    앱 완성

---
+++ SwiftUITutorials/Views/PageView/PageView.swift
@@ -0,0 +1,27 @@
+//
+//  PageView.swift
+//  SwiftUITutorials
+//
+//  Created by KIBEOM SHIN on 7/17/24.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
